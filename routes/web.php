4ee93480b2b3c01b739c942bc7d9ddbdf830a518
allinguaproject <schwarz.jakob.jan@zohu.eu>
2019-01-20 19:23:11 +0100

    ...

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,20 @@ Route::post('/load/sidebar', 'HomeController@loadSideBar')->name('load.sideBar')
 /*
     Sites available for  admin
 */
-Auth::routes();
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 
 
